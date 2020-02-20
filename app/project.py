29df037bfee85688f36f7f4c3d4f731753e80e5e
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-19 19:24:13 -0800

    created initial import of csv and ran the application

--- app/project.py
+++ app/project.py
@@ -1,1 +1,16 @@
 import csv
+
+def read_tasks(project_planner):
+    tasks = {}
+    for row in csv.reader(open(project_planner)):
+        number = row[0]
+        title = row[1]
+        duration = row[2]
+        prerequisites = row[3]
+        tasks[number] = (title, duration, prerequisites)
+
+    return tasks
+
+execute = read_tasks("planner.csv")
+
+print("To run the application, type 'execute'.")
