de1b5dd9403850db32de8432b6240d50eac67381
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-20 07:58:36 -0800

    completed the loops to mark day as complete and move down the task list

--- app/project.py
+++ app/project.py
@@ -20,9 +20,46 @@ print("To run the application, type 'tasks'.")
 
 
 def order_tasks(tasks):
+    # Start the loop with all tasks marked as incomplete on day zero
+    
     incomplete = set(tasks)
     completed = set()
     start_days = {}
+
+    # Begin the while and for loops to loop through all incomplete tasks
     while incomplete:
         for task_number in incomplete:
+
+            # check to make sure the prerequisites are completed for the task
+            task = tasks[task_number]
+            if task.prerequisites.issubset(completed):
+
+                # Create a loop to identify the day the task can be started
+                earliest_start_day = 0
+                for prereq_number in task.prerequisites:
+                    prereq_end_day = start_days[prereq_number] + \
+                                     tasks[prereq_number].duration
+
+                    # Once prereq is completed, the day the task is completed becomes the new earliest start day
+                    if prereq_end_day > earliest_start_day:
+                        earliest_start_day = prereq_end_day
+
+                    # Store the new earliest start day and mark the task as completed
+                    start_days[task_number] = earliest_start_day
+                    incomplete.remove(task_number)
+                    completed.add(task_number)
+
+                    break
+    # return the computed dictionary
+    return start_days
+                        
+
+
+
+
+
+
+
+
+
             
