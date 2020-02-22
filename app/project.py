73450e9e9ff794516a8448ee815c7b931f4f11ac
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-21 18:15:04 -0800

    transferring the first function called read_tasks

--- app/project.py
+++ app/project.py
@@ -3,3 +3,14 @@ from collections import namedtuple
 
 Task = namedtuple("Task", ["title", "duration", "prerequisites"])
 
+def read_tasks(planner):
+    tasks = {}
+    for row in csv.reader(open(planner, encoding='utf-8-sig')):
+        number = int(row[0])
+        title = row[1]
+        duration = float(row[2])
+        prerequisites = set(map(int, row[3].split()))
+        tasks[number] = Task(title, duration, prerequisites)
+    
+    return tasks
+
